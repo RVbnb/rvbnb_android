c8be5fc005c3299519f11b443dd6a4558873c057
msmaitran <itsmaisn@gmail.com>
2019-09-23 11:54:37 -0700

    Created Intents for the buttons.

--- app/src/main/java/com/example/rvbnb/RegisterActivity.kt
+++ app/src/main/java/com/example/rvbnb/RegisterActivity.kt
@@ -1,12 +1,20 @@
 package com.example.rvbnb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        // When user clicks on Submit button it will take user back to Login Page to login.
+        btn_submit.setOnClickListener {
+            var registrationIntent = Intent(this, LoginActivity::class.java)
+            startActivity(registrationIntent)
+        }
     }
 }
