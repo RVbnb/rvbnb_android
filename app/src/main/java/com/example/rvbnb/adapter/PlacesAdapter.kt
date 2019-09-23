0da2b3440e8e05bf45700fe95c5e67539dc42b39
Justin Gent <justinbgent@gmail.com>
2019-09-23 16:37:20 -0600

    Create adapters for Places that can be reserved and ReservedPlaces. Add item layouts for both Recyclers.

---
+++ app/src/main/java/com/example/rvbnb/adapter/PlacesAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.rvbnb.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rvbnb.R
+import com.example.rvbnb.model.Plot
+import kotlinx.android.synthetic.main.places_item.view.*
+
+class PlacesAdapter(private val reservedPlaces: MutableList<Plot>): RecyclerView.Adapter<PlacesAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val text: TextView = view.text_place
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.reserved_places_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = reservedPlaces.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var place = reservedPlaces[position]
+
+        holder.text.text = place.name
+    }
+}
