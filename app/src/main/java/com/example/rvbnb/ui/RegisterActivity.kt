314845ab0f1a7b46b98b534d92af1314a70fd993
msmaitran <itsmaisn@gmail.com>
2019-09-24 15:13:58 -0700

    Fixed bug on Registration Page.

--- app/src/main/java/com/example/rvbnb/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/rvbnb/ui/RegisterActivity.kt
@@ -3,6 +3,7 @@ package com.example.rvbnb.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.rvbnb.R
 import kotlinx.android.synthetic.main.activity_register.*
 
@@ -15,9 +16,23 @@ class RegisterActivity : AppCompatActivity() {
         // When user clicks on Submit button it will take user back to Login Page to login.
         btn_submit.setOnClickListener {
 
+            // If the Check Box for Landowner is checked, it will take user to the Login Page.
+            if (cb_register_landowner.isChecked && !cb_register_rvowner.isChecked) {
+                val registrationIntent = Intent(this, LoginActivity::class.java)
+                startActivity(registrationIntent)
+                // If the Check Box for RV Owner is checked, it will take user to the Login Page.
+            } else if (cb_register_rvowner.isChecked && !cb_register_landowner.isChecked) {
+                val registrationIntent = Intent(this, LoginActivity::class.java)
+                startActivity(registrationIntent)
+                // If both Check Boxes are selected, it will make a Toast and not take the user to the Login Page.
+            } else if (cb_register_landowner.isChecked && cb_register_rvowner.isChecked) {
+                Toast.makeText(this, "Please only select one", Toast.LENGTH_LONG).show()
+                // If neither Check Boxes are selected, it will make a Toast and not take the user to the Login Page.
+            } else {
+                Toast.makeText(this, "Please select Landowner or RV Owner", Toast.LENGTH_LONG).show()
+            }
+
             // TODO: Need to pass registration information to Profile Page.
-            val registrationIntent = Intent(this, LoginActivity::class.java)
-            startActivity(registrationIntent)
         }
     }
 }
