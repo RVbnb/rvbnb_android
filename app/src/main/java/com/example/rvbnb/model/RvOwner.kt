beb5e955c128054c0a6bdefc61e3c801ce6bc7a5
Justin Gent <justinbgent@gmail.com>
2019-09-24 16:00:31 -0600

    Refactor model class name to UserAccount

--- app/src/main/java/com/example/rvbnb/model/RvOwner.kt
+++
@@ -1,6 +0,0 @@
-package com.example.rvbnb.model
-
-class RvOwner(val username: String,
-              val password: String,
-              val is_land_owner: Boolean,
-              var reservedLocations: MutableList<Land>?)
--- app/src/main/java/com/example/rvbnb/retro/RvApi.kt
+++ app/src/main/java/com/example/rvbnb/retro/RvApi.kt
@@ -2,17 +2,16 @@ package com.example.rvbnb.retro
 
 import com.example.rvbnb.model.AcceptResponse
 import com.example.rvbnb.model.Land
-import com.example.rvbnb.model.LandOwner
-import com.example.rvbnb.model.RvOwner
+import com.example.rvbnb.model.UserAccount
 import retrofit2.Call
 import retrofit2.http.*
 
 interface RvApi {
     @POST("/api/auth/register")
-    fun registerUser(@Body landOwner: LandOwner)//: Call<LandOwner>
+    fun registerUser(@Body user: UserAccount)//: Call<UserAccount>
 
     @POST("/api/auth/login")
-    fun loginUser(@Body landOwner: LandOwner): Call<AcceptResponse>
+    fun loginUser(@Body user: UserAccount): Call<AcceptResponse>
 
     @POST(" /api/listings")
     fun registerLand(@Body land: Land,
