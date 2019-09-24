f561a67f4e77cddfe655ebd2b73b8f86389d1aca
msmaitran <itsmaisn@gmail.com>
2019-09-24 15:59:55 -0700

    Updated activity_create_listing file fixed the add image functionality in the CreateListingActivity file.

--- app/src/main/java/com/example/rvbnb/ui/CreateListingActivity.kt
+++ app/src/main/java/com/example/rvbnb/ui/CreateListingActivity.kt
@@ -1,14 +1,16 @@
 package com.example.rvbnb.ui
 
-import android.app.DatePickerDialog
+import android.annotation.SuppressLint
 import android.content.Intent
-import android.icu.util.Calendar
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.rvbnb.R
 import kotlinx.android.synthetic.main.activity_create_listing.*
 
 class CreateListingActivity : AppCompatActivity() {
+
+    var photoUri: Uri? = null
 
     companion object {
         internal const val REQUEST_IMAGE_GET = 1
@@ -22,8 +24,11 @@ class CreateListingActivity : AppCompatActivity() {
         iv_listing.setOnClickListener {
             val intent = Intent(Intent.ACTION_GET_CONTENT)
             intent.type = "image/*"
-            startActivityForResult(intent, REQUEST_IMAGE_GET)
-
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivityForResult(intent,
+                    REQUEST_IMAGE_GET
+                )
+            }
         }
 
         // When users clicks on Add Listing, user will be brought back to the Homepage.
@@ -32,6 +37,16 @@ class CreateListingActivity : AppCompatActivity() {
             // TODO: Need to have data that was entered to be saved and populated on the Homepage.
             val addListingIntent = Intent(this, LandownerActivity::class.java)
             startActivity(addListingIntent)
+        }
+    }
+
+    // The photo that was selected by the user will be set.
+    @SuppressLint("MissingSuperCall")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_GET && resultCode == RESULT_OK) {
+            val uri = data!!.data
+            photoUri = uri
+            iv_listing.setImageURI(uri)
         }
     }
 }
