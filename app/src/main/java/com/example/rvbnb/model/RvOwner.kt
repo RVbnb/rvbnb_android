613fdb135e85b46826c9a6c06e985f28ffd9d673
Justin Gent <justinbgent@gmail.com>
2019-09-24 14:27:09 -0600

    Correct model variable types to match JSON.

--- app/src/main/java/com/example/rvbnb/model/RvOwner.kt
+++ app/src/main/java/com/example/rvbnb/model/RvOwner.kt
@@ -3,6 +3,4 @@ package com.example.rvbnb.model
 class RvOwner(val username: String,
               val password: String,
               val is_land_owner: Boolean,
-              var displayName: String,
-              var reserveRequests: MutableList<Land>,
-              var reservedLocations: MutableList<Land>)
+              var reservedLocations: MutableList<Land>?)
