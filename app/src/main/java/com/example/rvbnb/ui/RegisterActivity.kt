7aaabc6bbf635185b79840e071add3f97a05dbea
Justin Gent <justinbgent@gmail.com>
2019-09-25 15:58:11 -0600

    Move register checkbox logic to viewModel.

--- app/src/main/java/com/example/rvbnb/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/rvbnb/ui/RegisterActivity.kt
@@ -23,21 +23,7 @@ class RegisterActivity : AppCompatActivity() {
 
             registerViewModel.register(et_email.text.toString(), et_register_password.text.toString(), cb_register_landowner.isChecked)
 
-            // If the Check Box for Landowner is checked, it will take user to the Login Page.
-            if (cb_register_landowner.isChecked && !cb_register_rvowner.isChecked) {
-                val registrationIntent = Intent(this, LoginActivity::class.java)
-                startActivity(registrationIntent)
-                // If the Check Box for RV Owner is checked, it will take user to the Login Page.
-            } else if (cb_register_rvowner.isChecked && !cb_register_landowner.isChecked) {
-                val registrationIntent = Intent(this, LoginActivity::class.java)
-                startActivity(registrationIntent)
-                // If both Check Boxes are selected, it will make a Toast and not take the user to the Login Page.
-            } else if (cb_register_landowner.isChecked && cb_register_rvowner.isChecked) {
-                Toast.makeText(this, "Please only select one", Toast.LENGTH_LONG).show()
-                // If neither Check Boxes are selected, it will make a Toast and not take the user to the Login Page.
-            } else {
-                Toast.makeText(this, "Please select Landowner or RV Owner", Toast.LENGTH_LONG).show()
-            }
+            registerViewModel.checkCheckboxes(cb_register_landowner, cb_register_rvowner)
 
             // TODO: Need to pass registration information to Profile Page.
             // TODO: Check to make sure passwords match.
--- app/src/main/java/com/example/rvbnb/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/example/rvbnb/viewmodel/RegisterViewModel.kt
@@ -1,13 +1,35 @@
 package com.example.rvbnb.viewmodel
 
 import android.content.Context
+import android.content.Intent
+import android.widget.CheckBox
+import android.widget.Toast
 import com.example.rvbnb.repo.LoginRepo
+import com.example.rvbnb.ui.LoginActivity
 
-class RegisterViewModel(context: Context) {
+class RegisterViewModel(private val context: Context) {
 
     private val loginRepo = LoginRepo(context)
 
     fun register(username: String, password: String, isLandOwner: Boolean){
         loginRepo.createUserAccount(username, password, isLandOwner)
+    }
+
+    fun checkCheckboxes(cbLandowner: CheckBox, cbRvOwner: CheckBox){
+        // If the Check Box for Landowner is checked, it will take user to the Landowner Homepage.
+        if (cbLandowner.isChecked && !cbRvOwner.isChecked) {
+            val registrationIntent = Intent(context, LoginActivity::class.java)
+            context.startActivity(registrationIntent)
+            // If the Check Box for RV Owner is checked, it will take user to the RV Owner Homepage.
+        } else if (cbRvOwner.isChecked && !cbLandowner.isChecked) {
+            val registrationIntent = Intent(context, LoginActivity::class.java)
+            context.startActivity(registrationIntent)
+            // If both Check Boxes are selected, it will make a Toast and not take the user to the Homepage.
+        } else if (cbLandowner.isChecked && cbRvOwner.isChecked) {
+            Toast.makeText(context, "Please only select one", Toast.LENGTH_LONG).show()
+            // If neither Check Boxes are selected, it will make a Toast and not take the user to the Homepage.
+        } else {
+            Toast.makeText(context, "Please select Landowner or RV Owner", Toast.LENGTH_LONG).show()
+        }
     }
 }
