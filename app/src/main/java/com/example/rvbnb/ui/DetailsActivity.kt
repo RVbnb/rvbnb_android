7b9c6feeeef2c70bda61d8320b077ec4d19e267d
msmaitran <itsmaisn@gmail.com>
2019-09-25 07:35:39 -0700

    Added delete functionality and upload image functionality to the Details page.

--- app/src/main/java/com/example/rvbnb/ui/DetailsActivity.kt
+++ app/src/main/java/com/example/rvbnb/ui/DetailsActivity.kt
@@ -1,5 +1,6 @@
 package com.example.rvbnb.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -20,10 +21,37 @@ class DetailsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_details)
         title = "Land Details"
 
+        // When user clicks on photo, user can select another photo to update with.
+        iv_listing_details.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivityForResult(intent,
+                    REQUEST_IMAGE_GET
+                )
+            }
+        }
+
         // When users clicks on Cancel, user will be brought back to the Homepage.
         btn_listing_cancel_details.setOnClickListener {
-            val cancelIntent = Intent(this, CreateListingActivity::class.java)
+            val cancelIntent = Intent(this, LandownerActivity::class.java)
             startActivity(cancelIntent)
+        }
+
+        btn_listing_delete_details.setOnClickListener {
+            // TODO: .removeAt(id)
+            val deleteIntent = Intent(this, LandownerActivity::class.java)
+            startActivity(deleteIntent)
+        }
+    }
+
+    // The photo that was selected by the user will be set.
+    @SuppressLint("MissingSuperCall")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_GET && resultCode == RESULT_OK) {
+            val uri = data!!.data
+            photoUri = uri
+            iv_listing_details.setImageURI(uri)
         }
     }
 }
