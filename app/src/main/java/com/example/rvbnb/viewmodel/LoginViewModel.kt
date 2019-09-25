dfedaa8756fa60123072806e2d001bc63e71f984
Justin Gent <justinbgent@gmail.com>
2019-09-25 11:36:02 -0600

    Add login functions to LoginViewModel and LoginActivity

--- app/src/main/java/com/example/rvbnb/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/rvbnb/viewmodel/LoginViewModel.kt
@@ -1,12 +1,17 @@
 package com.example.rvbnb.viewmodel
 
+import android.content.Context
 import android.view.View
+import androidx.lifecycle.ViewModel
+import com.example.rvbnb.repo.LandRepo
 
-class LoginViewModel {
+class LoginViewModel(context: Context): ViewModel() {
 
-    fun login(view: View){
-        view.setOnClickListener {
+    val landRepo = LandRepo(context)
 
-        }
+
+
+    fun login(username: String, password: String, context: Context){
+        landRepo.loginUser(username, password, context)
     }
 }
