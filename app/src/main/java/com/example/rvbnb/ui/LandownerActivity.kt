917317fcac4d586bbedadde7cd14a1f92c01263a
msmaitran <itsmaisn@gmail.com>
2019-09-25 20:39:13 -0700

    Made a change on the activity_landowner.xml file.

--- app/src/main/java/com/example/rvbnb/ui/LandownerActivity.kt
+++ app/src/main/java/com/example/rvbnb/ui/LandownerActivity.kt
@@ -4,10 +4,36 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.rvbnb.R
+import com.example.rvbnb.adapter.PlacesAdapter
+import com.example.rvbnb.model.Land
+import com.example.rvbnb.repo.LoginRepo
 import kotlinx.android.synthetic.main.activity_landowner.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class LandownerActivity : AppCompatActivity() {
+class LandownerActivity : AppCompatActivity(), Callback<Land>, LoginRepo.GetLandListCallback {
+
+    override fun getList(mutableList: MutableList<Land>) {
+        recyclerSetup(mutableList)
+    }
+
+    private fun recyclerSetup(list: MutableList<Land>){
+        rv_places_item.setHasFixedSize(true)
+        val manager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        rv_places_item.layoutManager = manager
+        rv_places_item.adapter = PlacesAdapter(list)
+    }
+
+    override fun onFailure(call: Call<Land>, t: Throwable) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onResponse(call: Call<Land>, response: Response<Land>) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
