a16cb7612d76c033cc12c2631ebf37b202559c68
msmaitran <itsmaisn@gmail.com>
2019-09-25 21:38:50 -0700

    Updated the activity_login.xml file to have a show/hide button as well as hide the password as the user types it in. Added the show/hide button functionality to the login page.

--- app/src/main/java/com/example/rvbnb/ui/LoginActivity.kt
+++ app/src/main/java/com/example/rvbnb/ui/LoginActivity.kt
@@ -3,6 +3,8 @@ package com.example.rvbnb.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.widget.Toast
 import com.example.rvbnb.R
 import com.example.rvbnb.model.AcceptResponse
@@ -31,6 +33,19 @@ class LoginActivity : AppCompatActivity(), LoginRepo.LoginResponseCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         loginViewModel = LoginViewModel(this)
+
+        // Password is autmatically hidden as user types it in.
+        // When user clicks on the Show, it will show the password.
+        // when user clicks on Hide, it will hide password.
+        btn_show_hide.setOnClickListener {
+            if (btn_show_hide.text.toString().equals("Show")) {
+                et_password.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                btn_show_hide.text = "Hide"
+            } else {
+                et_password.transformationMethod = PasswordTransformationMethod.getInstance()
+                btn_show_hide.text = "Show"
+            }
+        }
 
         // When user clicks on Log-In button with valid credentials, it will take user to Homepage.
         btn_login.setOnClickListener {
