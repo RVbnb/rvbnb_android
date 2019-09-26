78435256315f3f9479eb4cf8ceba36937265094e
Justin Gent <justinbgent@gmail.com>
2019-09-25 18:32:35 -0600

    Touch up updateLand method and add getReservations method to RvApi. Add Reservations class to model and adjusted TimeSlot class.

--- app/src/main/java/com/example/rvbnb/retro/RvApi.kt
+++ app/src/main/java/com/example/rvbnb/retro/RvApi.kt
@@ -2,6 +2,7 @@ package com.example.rvbnb.retro
 
 import com.example.rvbnb.model.AcceptResponse
 import com.example.rvbnb.model.Land
+import com.example.rvbnb.model.Reservations
 import com.example.rvbnb.model.UserAccount
 import retrofit2.Call
 import retrofit2.http.*
@@ -20,8 +21,13 @@ interface RvApi {
     @GET("/api/listings")
     fun getLandList(@Header("Authorization")token: String): Call<MutableList<Land>>
 
+    @GET("/api/listings/{id}")
+    fun getReservations(@Header("Authorization")token: String,
+                        @Path("id")id: Int): Call<Reservations>
+
     @PUT("/api/listings/{id}")
     fun updateLand(@Header("Authorization")token: String,
+                   @Path("id")id: Int,
                    @Body land: Land): Call<Void>
 
     @DELETE("/api/listings/{id}")
